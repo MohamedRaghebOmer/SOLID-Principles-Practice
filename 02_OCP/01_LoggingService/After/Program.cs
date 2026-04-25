3ba5f2bf92b258e246874d6e1c9f6fc278277bf2
Mohamed Ragheb <mohamedraghebomer@gmail.com>
2026-04-25 22:16:40 +0300

    Learn & Practice: OCP

---
+++ 02_OCP/01_LoggingService/After/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+public sealed class LoggingService<T>
+{
+    private readonly ILogger<T> _logger;
+
+    public void Log(T value)
+    {
+        _logger.Log(value);
+    }
+
+    public LoggingService(ILogger<T> logger)
+    {
+        this._logger = logger;
+    }
+}
+
+public interface ILogger<T>
+{
+    void Log(T value);
+}
+
+public class FileLogger<T> : ILogger<T>
+{
+    // Method to log to file
+    public void Log(T value)
+    {
+        Console.WriteLine("\nLog to file: {0}", value?.ToString());
+    }
+}
+
+public class EventViewerLogger<T> : ILogger<T>
+{
+    // Method to log to EventLog
+    public void Log(T value)
+    {
+        Console.WriteLine("\nLog to Event Log: {0}", value?.ToString());
+    }
+}
+
+public class DatabaseLogger<T> : ILogger<T>
+{
+    // Method to log to file
+    public void Log(T value)
+    {
+        Console.WriteLine("\nLog to Database: {0}", value?.ToString());
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Log to file
+        LoggingService<string> fileServiceLogger = new LoggingService<string>(new FileLogger<string>());
+        fileServiceLogger.Log("This message will be logged in a file");
+
+        // Log to viewer
+        LoggingService<string> eventViewerServiceLogger = new LoggingService<string>(new EventViewerLogger<string>());
+        eventViewerServiceLogger.Log("This message will be logged windows viewer.");
+
+        // Log to database
+        LoggingService<string> dbServiceLogger = new LoggingService<string>(new DatabaseLogger<string>());
+        dbServiceLogger.Log("This message will be logged windows viewer.");
+    }
+}
---
+++ 02_OCP/02_PaymentService/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+public sealed class PaymentService
+{
+    private readonly IPayment _payment;
+
+    public void Pay(decimal amount)
+    {
+        _payment.Pay(amount);
+    }
+
+    public PaymentService(IPayment payment)
+    {
+        this._payment = payment;
+    }
+}
+
+public interface IPayment
+{
+    void Pay(decimal amount);
+}
+
+public class PayPal : IPayment
+{
+    public void Pay(decimal amount)
+    {
+        Console.WriteLine("PayPal: Amount:[{0:C}] paid successfully.", amount);
+    }
+}
+
+public class CreditCard : IPayment
+{
+    public void Pay(decimal amount)
+    {
+        Console.WriteLine("CreditCard: Amount:[{0:C}] paid successfully.", amount);
+    }
+}
+
+public class BankTransfer : IPayment
+{
+    public void Pay(decimal amount)
+    {
+        Console.WriteLine("BankTransfer: Amount:[{0:C}] paid successfully.", amount);
+    }
+}
+
+public class BitCoin : IPayment
+{
+    public void Pay(decimal amount)
+    {
+        Console.WriteLine("BitCoin: Amount:[{0:C}] paid successfully.", amount);
+    }
+}
+
+internal class PaymentServiceExample
+{
+    static void Main(string[] args)
+    {
+        // PayPal Payment
+        PaymentService paymentService = new PaymentService(new PayPal());
+        paymentService.Pay(100m);
+
+        // Credit Card Payment
+        paymentService = new PaymentService(new CreditCard());
+        paymentService.Pay(100m);
+
+        // Bank Transfer Payment
+        paymentService = new PaymentService(new BankTransfer());
+        paymentService.Pay(100m);
+
+        // Bit Coin Payment
+        paymentService = new PaymentService(new BitCoin());
+        paymentService.Pay(100m);
+    }
+}
