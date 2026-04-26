b1330ba9016960ddb92d27ef334284b6341f1830
Mohamed Ragheb <mohamedraghebomer@gmail.com>
2026-04-26 17:24:38 +0300

    Learn & Practice: LSP

---
+++ 03_LSP/02_Vehicle/After/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+public class Vehicle
+{
+    public virtual void Drive()
+    {
+        Console.WriteLine("Vehicle is driving...");
+    }
+}
+
+public class VehiclesWithEngine : Vehicle
+{
+    public virtual void StartEngine()
+    {
+        Console.WriteLine("Starting Engine...");
+    }
+}
+
+public class Car : VehiclesWithEngine
+{
+    public override void Drive()
+    {
+        Console.WriteLine("Car is driving...");
+    }
+
+    public override void StartEngine()
+    {
+        Console.WriteLine("Care is starting the engine...");
+    }
+}
+
+public class Bicycle : Vehicle
+{
+    public override void Drive()
+    {
+        Console.WriteLine("Bicycle is driving...");
+    }
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Car car = new Car();
+
+        car.Drive(); // Output: care is driving
+        car.StartEngine(); // Output: Care is starting the engine...
+
+
+        Bicycle bicycle = new Bicycle();
+
+        bicycle.Drive(); // Bicycle is driving...
+    }
+}
---
+++ 03_LSP/03_Person/After/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace After
+{
+    public class Person
+    {
+        // Virtual method to send an email
+        public virtual void SendEmail()
+        {
+            Console.WriteLine("Sending email to a person...");
+        }
+    }
+
+    public class Employee : Person
+    {
+        // Override PaySalary for employees
+        public virtual void PaySalary()
+        {
+            Console.WriteLine("Paying salary to an employee...");
+        }
+
+        // Override SendEmail for employees
+        public override void SendEmail()
+        {
+            Console.WriteLine("Sending email to an employee...");
+        }
+    }
+
+    public class Manager : Employee
+    {
+        // Override PaySalary for managers
+        public override void PaySalary()
+        {
+            Console.WriteLine("Paying salary to a manager...");
+        }
+
+        // Override SendEmail for managers
+        public override void SendEmail()
+        {
+            Console.WriteLine("Sending email to a manager...");
+        }
+    }
+
+    public class Student : Person
+    {
+        // Override SendEmail for students
+        public override void SendEmail()
+        {
+            Console.WriteLine("Sending email to a student...");
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee();
+            employee1.PaySalary();   // Calls PaySalary method for Employee
+            employee1.SendEmail();   // Calls SendEmail method for Employee
+
+            Employee employee2 = new Manager();
+            employee2.PaySalary();   // Calls PaySalary method for Manager
+            employee2.SendEmail();   // Calls SendEmail method for Manager
+
+            Person student1 = new Student();
+            student1.SendEmail();   // Calls SendEmail method for Student
+        }
+    }
+}
